1760e5806abe091c18c7bc02ebe07d72d31a2006
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2018-02-19 15:46:40 +0100

    name is important

--- restoreTensors.py
+++ restoreTensors.py
@@ -4,7 +4,37 @@ Created on Mon Feb 19 13:02:05 2018
 
 @author: B51427
 """
+import tensorflow as tf  
+import numpy as np 
+from tensorflow.python.tools import inspect_checkpoint as chkp
 
-def restore_tensor():
+tf.reset_default_graph()  
+imported_meta = tf.train.import_meta_graph("model_final.meta")
+
+with tf.Session() as sess:  
+    imported_meta.restore(sess, tf.train.latest_checkpoint('./'))
+    print(tf.__version__)
+    chkp.print_tensors_in_checkpoint_file(file_name="./model_final", tensor_name='', all_tensors=True)#for tf v1.5#, all_tensor_names=True)
+    graph = tf.get_default_graph()
+    h_est2 = graph.get_tensor_by_name('hor_estimate:0')
+    v_est2 = graph.get_tensor_by_name('ver_estimate:0')
+#    print("h_est: %.2f, v_est: %.2f" % (h_est2, v_est2))
+    print(h_est2, v_est2)
+    # Access saved Variables directly
+    print(sess.run('bias:0'))
+    # This will print 2, which is the value of bias that we saved
     
-restore_tensor()
+    
+    # Now, let's access and create placeholders variables and
+    # create feed-dict to feed new data
+    #graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name("w1:0")
+    w2 = graph.get_tensor_by_name("w2:0")
+    w3 = graph.get_tensor_by_name("w3:0")
+    feed_dict ={w1:13.0,w2:17.0}
+    
+    #Now, access the op that you want to run. 
+    op_to_restore = graph.get_tensor_by_name("op_to_restore:0")
+    print(sess.run(w3, feed_dict))
+    print(sess.run(op_to_restore,feed_dict))
+    #This will print 60 which is calculated 
--- saveTensors.py
+++ saveTensors.py
@@ -4,8 +4,85 @@ Created on Mon Feb 19 13:00:11 2018
 
 @author: Saad
 """
+import tensorflow as tf  
+import numpy as np 
+import matplotlib.pyplot as plt  
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
 
-def save_tensors():
-    
+# Clear the current graph in each run, to avoid variable duplication
+tf.reset_default_graph()
 
-save_tensors()
+# Create placeholders for the x and y points
+X = tf.placeholder("float")  
+Y = tf.placeholder("float")
+
+# Initialize the two parameters that need to be learned
+h_est = tf.Variable(0.0, name='hor_estimate')  
+v_est = tf.Variable(0.0, name='ver_estimate')
+
+# y_est holds the estimated values on y-axis
+y_est = tf.square(X - h_est) + v_est
+
+# Define a cost function as the squared distance between Y and y_est
+cost = (tf.pow(Y - y_est, 2))
+
+# The training operation for minimizing the cost function. The
+# learning rate is 0.001
+trainop = tf.train.GradientDescentOptimizer(0.001).minimize(cost) 
+
+# Use some values for the horizontal and vertical shift
+h = 1  
+v = -2
+
+# Generate training data with noise
+x_train = np.linspace(-2,4,201)  
+noise = np.random.randn(*x_train.shape) * 0.4  
+y_train = (x_train - h) ** 2 + v + noise
+
+# Visualize the data 
+plt.rcParams['figure.figsize'] = (10, 6)  
+plt.scatter(x_train, y_train)  
+plt.xlabel('x_train')  
+plt.ylabel('y_train')  
+
+##################################################################
+#Prepare to feed input, i.e. feed_dict and placeholders
+w1 = tf.placeholder("float", name="w1")
+w2 = tf.placeholder("float", name="w2")
+b1= tf.Variable(2.0,name="bias")
+feed_dict ={w1:4,w2:8}
+
+#Define a test operation that we will restore
+w3 = tf.add(w1,w2, name="w3")
+w4 = tf.multiply(w3,b1,name="op_to_restore")
+#################################################################
+
+t_est = tf.Variable(10.0, name='t_estimate')
+h_ = tf.placeholder("float", name = "h_")
+init = tf.global_variables_initializer()
+
+# Run a session. Go through 100 iterations to minimize the cost
+def train_graph():  
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(100):
+            for (x, y) in zip(x_train, y_train):
+
+                # Feed actual data to the train operation
+                sess.run(trainop, feed_dict={X: x, Y: y})
+              
+        print(sess.run(w4,feed_dict))
+        h_ = sess.run(tf.multiply(2,4))
+        v_ = sess.run(v_est)
+        
+        # Create a Saver object
+        saver = tf.train.Saver()
+       
+        # Save the final model
+        saver.save(sess, './model_final')
+        
+    return h_, v_
+
+result = train_graph()  
+print("h_est = %.2f, v_est = %.2f" % result) 
