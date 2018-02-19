c9d36f8bdf1279288967166ba6157de593bb4d04
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2018-02-19 13:04:43 +0100

    dummy files to start with

---
+++ restoreTensors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 13:02:05 2018
+
+@author: B51427
+"""
+
+def restore_tensor():
+    
+restore_tensor()
---
+++ saveTensors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 13:00:11 2018
+
+@author: Saad
+"""
+
+def save_tensors():
+    
+
+save_tensors()
